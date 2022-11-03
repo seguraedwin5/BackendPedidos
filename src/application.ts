a1f8aefe2261cbe220154f96ab8c86866f2198af
seguraedwin5 <51142779+seguraedwin5@users.noreply.github.com>
2022-11-02 19:56:16 -0500

    adicion servicio estrategias

--- src/application.ts
+++ src/application.ts
@@ -1,13 +1,15 @@
+import {AuthenticationComponent, registerAuthenticationStrategy} from '@loopback/authentication';
 import {BootMixin} from '@loopback/boot';
 import {ApplicationConfig} from '@loopback/core';
-import {
-  RestExplorerBindings,
-  RestExplorerComponent,
-} from '@loopback/rest-explorer';
 import {RepositoryMixin} from '@loopback/repository';
 import {RestApplication} from '@loopback/rest';
+import {
+  RestExplorerBindings,
+  RestExplorerComponent
+} from '@loopback/rest-explorer';
 import {ServiceMixin} from '@loopback/service-proxy';
 import path from 'path';
+import {RolAdministrador} from './roles/admin.Strategy';
 import {MySequence} from './sequence';
 
 export {ApplicationConfig};
@@ -40,5 +42,8 @@ export class App extends BootMixin(
         nested: true,
       },
     };
+
+    registerAuthenticationStrategy(this, RolAdministrador);
+    this.component(AuthenticationComponent);
   }
 }
